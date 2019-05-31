e113098ec22034fbc483963498b3e07cd8933023
BAANG <chcelestino@gmail.com>
2019-05-31 14:26:02 -0700

    Add prompts, and constructor into game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+var countdown = 30;
+var questionArray = []; // TODO: Insert list of questions
+var correct = 0;
+var incorrect = 0;
+var questionNum = 1;
+
+// Constructor for Trivia Questions
+
+function makeQuestion(prompt, answer, image) {
+    this.prompt = currentQuestion;
+    this.answer = currentAnswer;
+    this.image = currentImage;
+}
+
+var questions = [
+    {prompt: "How many days are in a week?", answer: "7", image: "[INSERT IMG SRC HERE]"},
+    {prompt: "True or False"}
+]
